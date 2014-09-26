7b5fec7c5fc7823252d2929969610631d1cbb600
Mark Morgan <momipsl@ipsl.jussieu.fr>
2014-09-26 17:55:03 +0200

    Added ordered dictionary support

--- api/metric/examples/tos_2.5x2.5_esmf_linear_metrics-convertor.py
+++ api/metric/examples/tos_2.5x2.5_esmf_linear_metrics-convertor.py
@@ -6,9 +6,15 @@ import collections, json, os
 # Set path to metrics file.
 _FPATH = os.path.abspath(__file__).replace("-convertor.py", ".json")
 
-# Set input metrics.
+# Load input file.
 with open(_FPATH, 'r') as ifile:
-	metrics_in = json.loads(ifile.read(), object_pairs_hook=collections.OrderedDict)
+    data = ifile.read()
+
+# Replace NaN with "nan" as this is against JSON spec and fails jquery parsing.
+data = data.replace("NaN", '"nan"')
+
+# Set input metrics.
+metrics_in = json.loads(data, object_pairs_hook=collections.OrderedDict)
 
 # Set group.
 group = "tos_2-5x2-5_esmf_linear_metrics"
@@ -22,19 +28,19 @@ fields += metrics_in["GFDL-ESM2G"]["defaultReference"]["r1i1p1"]["global"].keys(
 # Set lines.
 lines = []
 for realm, metrics in metrics_in["GFDL-ESM2G"]["defaultReference"]["r1i1p1"].iteritems():
-	line = []
-	line += metrics_in["GFDL-ESM2G"]["SimulationDescription"].values()
-	line.append(realm)
-	line.append(metrics_in["GFDL-ESM2G"]["defaultReference"]["source"])
-	line.append(metrics_in["RegionalMasking"][realm])
-	line += metrics_in["GFDL-ESM2G"]["defaultReference"]["r1i1p1"]["global"].values()
-	lines.append(line)
+    line = []
+    line += metrics_in["GFDL-ESM2G"]["SimulationDescription"].values()
+    line.append(realm)
+    line.append(metrics_in["GFDL-ESM2G"]["defaultReference"]["source"])
+    line.append(metrics_in["RegionalMasking"][realm])
+    line += metrics_in["GFDL-ESM2G"]["defaultReference"]["r1i1p1"][realm].values()
+    lines.append(line)
 
 # Set output metrics.
 metrics_out = {
-	"group": group,
-	"columns": fields,
-	"metrics": lines
+    "group": group,
+    "columns": fields,
+    "metrics": lines
 }
 
 # Set output filename.
@@ -43,4 +49,4 @@ ofpath = "{0}-converted{1}".format(fname, fext)
 
 # Write output.
 with open(ofpath, 'w') as ofile:
-	ofile.write(json.dumps(metrics_out))
+    ofile.write(json.dumps(metrics_out))
